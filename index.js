6996a2e089e5f744648c807b37ddf5da6d8e1084
MatheusHSA <matheushsantosa@gmail.com>
2026-03-27 22:54:03 -0300

    feat: implementa rota POST para salvar filmes

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ app.get('/api/filmes', (req, res) => {
     res.json(produtos);
 });
 
+app.use(express.json()); // Necessário para ler o corpo da requisição
+
+app.post('/api/filmes', (req, res) => {
+    const novoFilme = req.body;
+    produtos.push(novoFilme);
+    res.status(201).json(novoFilme);
+});
+
 app.listen(port, () => console.log(`API rodando em http://localhost:${port}`));
